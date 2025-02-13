357c7245ba34e0a14cb05c3c2a0b044ac37daf18
Ngo Xuan Thiep <ngothiep2412@gmail.com>
2025-02-13 15:42:31 +0700

    feature(component): add book list

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
@@ -3,7 +3,6 @@ package com.plcoding.bookpedia
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,4 +24,10 @@ private fun BookSearchBarPreview() {
             modifier = Modifier.fillMaxWidth()
         )
     }
+}
+
+@Preview
+@Composable
+private fun BookListScreenView() {
+
 }
