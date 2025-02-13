b1238389481c1ad49af6e6dfc0fe508a98ceb37f
Ngo Xuan Thiep <ngothiep2412@gmail.com>
2025-02-13 15:31:15 +0700

    feature(component): add book list item

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -5,11 +5,17 @@ import com.plcoding.bookpedia.book.presentation.booklist.BookListScreenRoot
 import com.plcoding.bookpedia.book.presentation.booklist.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+/**
+ * 🧐
+ * Dùng remember để giữ lại một instance của BookListViewModel trong suốt vòng đời của composable.
+ * Tránh việc khởi tạo lại BookListViewModel() khi UI recompose.
+ */
 
 @Composable
 @Preview
 fun App() {
     BookListScreenRoot(
+
         viewModel = remember {  BookListViewModel() },
         onBookClick =  {}
     )
